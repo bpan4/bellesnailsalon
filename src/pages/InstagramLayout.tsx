ece2c0dff6a48f0bb0ad493f36decf50200893de
Belle Pan <belle.pan@mail.mcgill.ca>
2024-06-05 17:49:43 -0400

    fix: clicking on images now opens new page to view posts on instagram

--- src/pages/InstagramLayout.tsx
+++ src/pages/InstagramLayout.tsx
@@ -1,37 +1,48 @@
-import { InstagramLayoutProps } from "../resources/Interface";
+import { InstagramLayoutProps } from '../resources/Interface'
 
-const VIDEO_TYPE = "VIDEO";
+const VIDEO_TYPE = 'VIDEO'
 
-const InstagramLayout = ({ imageList } : InstagramLayoutProps) => {
+const InstagramLayout = ({ imageList }: InstagramLayoutProps) => {
     return (
-        <div className="media__container">
-            {imageList.map((image) => {
+        <div className='media__container'>
+            {imageList.map(image => {
                 if (image.mediaType === VIDEO_TYPE) {
                     return (
-                        <video
-                            controls
-                            muted
-                            autoPlay={true}
-                            loop
-                            controlsList="nodownload nofullscreen noremoteplayback"
+                        <a
+                            href={image.permalink}
                             key={image.permalink}
-                            src={image.mediaUrl}
-                            // alt="Instagram video from @nail.chimp"
-                            className="media"
-                        />
+                            target='_blank'
+                            rel='noreferrer'
+                        >
+                            <video
+                                controls
+                                muted
+                                autoPlay={true}
+                                loop
+                                controlsList='nodownload nofullscreen noremoteplayback'
+                                src={image.mediaUrl}
+                                className='media'
+                            />
+                        </a>
                     )
                 }
                 return (
-                    <img 
+                    <a
+                        href={image.permalink}
                         key={image.permalink}
-                        src={image.mediaUrl} 
-                        alt="Instagram post from @nail.chimp"
-                        className="media"
-                    />
+                        target='_blank'
+                        rel='noreferrer'
+                    >
+                        <img
+                            src={image.mediaUrl}
+                            alt='Instagram post from @nail.chimp'
+                            className='media'
+                        />
+                    </a>
                 )
             })}
         </div>
     )
 }
 
-export default InstagramLayout;
+export default InstagramLayout
